0ca959783644cb50199e921a5494feed658fd5f1
Bobby Nannier <47843158+bnannier@users.noreply.github.com>
2025-08-05 21:50:05 -0400

    Sync: Update src/hooks/useOfflineNotes.ts from bnannier/bright-scribe-application

--- src/hooks/useOfflineNotes.ts
+++ src/hooks/useOfflineNotes.ts
@@ -96,7 +96,7 @@ export const useOfflineNotes = () => {
         performSync();
       }
     }
-  }, [offlineStorage, user, loadNotes, performSync, isOnline]);
+  }, [offlineStorage, user, isOnline]); // Removed circular dependencies
 
   // Listen for network restoration
   useEffect(() => {
